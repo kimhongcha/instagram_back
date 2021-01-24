dfd5b4e7c2d4e4ab50de0a4f1fa33f07c830ca03
cheoryeon <rlacjfgus2@gmail.com>
2021-01-24 16:23:37 +0900

    11

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -5,12 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry cr) {
         cr.addMapping("/**")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedOrigins("http://127.0.0.1:8080")  // 허용할 주소 및 포트
                 .allowedOrigins("http://localhost:8080");  // 허용할 주소 및 포트
     }
